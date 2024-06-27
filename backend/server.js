d5708561601097e6c7fc77b2f4f04e1fe55c2e5e
Allan Rubio <rubio_allan@yahoo.com>
2024-06-26 21:04:20 -0700

    path added to server.js

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,6 @@ import addressRoutes from "./routes/addressRoutes.js";
 import paymentRoutes from "./routes/paymentRoutes.js";
 import { createTables, client } from "./db.js";
 import { authenticateToken } from "./middleware/authMiddleware.js";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// Define __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(
@@ -27,6 +21,7 @@ app.use(
   })
 );
 
+const path = require("path");
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "../client/dist/index.html"))
 );
