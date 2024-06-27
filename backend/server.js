62ee62522d287c44bfbec4d26108c97192d0f39c
Allan Rubio <rubio_allan@yahoo.com>
2024-06-26 21:46:38 -0700

    more updates to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
@@ -13,17 +15,16 @@ import paymentRoutes from "./routes/paymentRoutes.js";
 import { createTables, client } from "./db.js";
 import { authenticateToken } from "./middleware/authMiddleware.js";
 
+// Get the __dirname equivalent in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
-);
-
-import path from "path";
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"))
 );
 
 app.use(morgan("dev"));
@@ -39,6 +40,16 @@ app.use("/api/reviews", authenticateToken, reviewRoutes);
 app.use("/api/addresses", authenticateToken, addressRoutes);
 app.use("/api/payments", authenticateToken, paymentRoutes);
 
+// Serve static files
+app.use(
+  "/assets",
+  express.static(path.join(__dirname, "../client/dist/assets"))
+);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
 app.use((error, req, res, next) => {
   console.error(error.stack);
   res.status(500).send({ message: error.message });
@@ -48,9 +59,7 @@ const PORT = process.env.PORT || 3000;
 
 client
   .connect()
-  .then(() => {
-    return createTables();
-  })
+  .then(() => createTables())
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
