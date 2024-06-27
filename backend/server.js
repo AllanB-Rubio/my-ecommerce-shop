e6bee7998bce8c9d66459a8a467eb131f792584b
Allan Rubio <rubio_allan@yahoo.com>
2024-06-26 22:07:01 -0700

    can this deploy work now

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
@@ -15,16 +13,17 @@ import paymentRoutes from "./routes/paymentRoutes.js";
 import { createTables, client } from "./db.js";
 import { authenticateToken } from "./middleware/authMiddleware.js";
 
-// Get the __dirname equivalent in ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
+);
+
+import path from "path";
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"))
 );
 
 app.use(morgan("dev"));
@@ -40,11 +39,6 @@ app.use("/api/reviews", authenticateToken, reviewRoutes);
 app.use("/api/addresses", authenticateToken, addressRoutes);
 app.use("/api/payments", authenticateToken, paymentRoutes);
 
-// Serve the index.html file for any other requests
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
-
 app.use((error, req, res, next) => {
   console.error(error.stack);
   res.status(500).send({ message: error.message });
@@ -54,7 +48,9 @@ const PORT = process.env.PORT || 3000;
 
 client
   .connect()
-  .then(() => createTables())
+  .then(() => {
+    return createTables();
+  })
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
