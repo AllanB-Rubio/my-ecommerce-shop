948a56bd3af26b9e0a15445fc988147ad401c9bd
Allan Rubio <rubio_allan@yahoo.com>
2024-06-30 22:47:59 -0700

    Checkout component updated for deployment

--- src/pages/CheckoutOptions.jsx
+++ src/pages/CheckoutOptions.jsx
@@ -1,28 +1,68 @@
-// src/pages/CheckoutOptions.jsx
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./CheckoutOptions.css";
+// src/pages/Checkout.jsx
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useLocation } from "react-router-dom";
+import "./Checkout.css";
 
-const CheckoutOptions = () => {
+const Checkout = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const [cart, setCart] = useState([]);
+  const [isGuest, setIsGuest] = useState(false);
 
-  const handleGuestCheckout = () => {
-    navigate("/shipping-billing?guest=true");
+  const apiURL = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    const storedCart = JSON.parse(localStorage.getItem("cart")) || [];
+    setCart(storedCart);
+    const params = new URLSearchParams(location.search);
+    if (params.get("guest") === "true") {
+      setIsGuest(true);
+    }
+  }, [location.search]);
+
+  const handleOrder = async () => {
+    try {
+      const orderData = {
+        items: cart,
+        total_amount: cart.reduce(
+          (total, item) => total + item.price * item.quantity,
+          0
+        ),
+      };
+
+      if (isGuest) {
+        const response = await axios.post(
+          `${apiURL}/api/orders/guest`,
+          orderData
+        );
+        navigate(`/order-confirmation/${response.data.id}`);
+      } else {
+        const token = localStorage.getItem("token");
+        const userId = JSON.parse(atob(token.split(".")[1])).id; // Decode JWT to get userId
+        orderData.userId = userId;
+        const response = await axios.post(`${apiURL}/api/orders`, orderData, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        navigate(`/order-confirmation/${response.data.id}`);
+      }
+
+      // Clear cart after order is placed
+      setCart([]);
+      localStorage.removeItem("cart");
+    } catch (error) {
+      console.error("Failed to place order", error);
+    }
   };
 
   return (
-    <div className="checkout-options-container">
-      <h1>Checkout Options</h1>
-      <p>Please choose an option to proceed with your checkout:</p>
-      <div className="checkout-buttons">
-        <button onClick={() => navigate("/account?form=login")}>Login</button>
-        <button onClick={() => navigate("/account?form=register")}>
-          Register
-        </button>
-        <button onClick={handleGuestCheckout}>Checkout as Guest</button>
-      </div>
+    <div className="checkout-container">
+      <h1>Checkout</h1>
+      <button onClick={handleOrder}>Place Order</button>
     </div>
   );
 };
 
-export default CheckoutOptions;
+export default Checkout;
